5aa2344b7fe3f915d238d055e5e0f66f86d99fde
EternalCH <2987475289@qq.com>
2024-03-27 23:31:13 +0800

    add：添加了造成的伤害区间

--- TextGame.c
+++ TextGame.c
@@ -6,22 +6,19 @@
 
 int main()
 {
-    int player_max_hp = 100, moster_max_hp = 500, player_attack_damage, moster_attack_damage;
-    srand(time(NULL));
-    char name;
-    printf("请输入你的名字\n");
+    int player_max_hp = 100, moster_max_hp = 500;
+    srand((unsigned int) time(NULL));
+    char name[40];  printf("请输入你的名字\n");
     scanf("%s", name);
 
     while (1)
     {
-        player_attack_damage = PLAYER_ATTACK_POWER * (rand() % 4 + 3);
-        moster_max_hp -= player_attack_damage;
-        printf("%s进行了攻击,玩家造成了%d伤害\n", name, player_attack_damage);
+        printf("%s进行了攻击,玩家造成了%d伤害\n", name,get_harm(PLAYER_ATTACK_POWER));
+        moster_max_hp-=get_harm(PLAYER_ATTACK_POWER);
         printf("怪物剩余%d血量\n", moster_max_hp);
-        moster_attack_damage = MOSTER_ATTACK_POWER * (rand() % 4 + 3);
-        player_max_hp -= moster_attack_damage;
-        printf("怪物进行了攻击,造成了%d伤害\n", moster_attack_damage);
-        printf("玩家剩余%d血量", player_max_hp);
+        printf("怪物进行了攻击,造成了%d伤害\n", get_harm(MOSTER_ATTACK_POWER));
+        player_max_hp-=get_harm(MOSTER_ATTACK_POWER);
+        printf("玩家剩余%d血量\n", player_max_hp);
         if (player_max_hp <= 0)
         {
             printf("你失败了\n");
@@ -36,6 +33,13 @@ int main()
     return 0;
 }
 
-int get_harm()
+int get_harm(int a)
 {
+    double num;
+    double down=0.8,top=1.2;
+    double b;
+    num=(double)rand()/RAND_MAX;
+    b=down+num*(down-top);
+    a*=b;
+    return a;
 }
